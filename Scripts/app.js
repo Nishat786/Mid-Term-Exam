6fa15609400ab4ffd3830af6536031f0c25c7c47
nbagga1 <300983408@cencol.ca>
2018-06-20 11:16:26 -0400

    Added contents to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,14 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
 "use strict";
+let SubmitButton = document.getElementById("SendButton");
+        SubmitButton.addEventListener("click", (event) => {
 
-
-// YOUR CODE GOES HERE
-
+            if (!document.forms[0].checkValidity()) {
+                OutputFormDataToConsole();
+                ValidateForm();
+            }
+        })
 
 })();
 
