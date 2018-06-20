b2bbf80b57ec102f854ae4fee807c6c764ec8bac
nbagga1 <300983408@cencol.ca>
2018-06-20 12:09:39 -0400

    Added Paragraphs in Javascript

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,4 +1,8 @@
 // setup your IIFE (Immediately Invoked Function Expression)
+// Mid-Term - Bagga - Nishat
+// Nishat Bagga
+// All Javascript files goes here 
+
 (function (content) {
     let FullName = document.getElementById("FullName");
     let ContactNumber = document.getElementById("ContactNumber");
@@ -27,6 +31,42 @@
         }
 
     }
+
+    (function Spetses() {
+        <div id="pcontainer">
+  <p> Spetses is an affluent island and a municipality in the Islands regional unit,
+                             Attica, Greece.[2] It is sometimes included as one of the Saronic Islands.
+                              Until 1948, it was part of the old prefecture of Argolidocorinthia, which
+                               is now split into Argolis and Corinthia.</p>
+</div>
+    })
+    (function Folegandros() {
+        <div id="pcontainer">
+  <p>Folegandros is a small Greek island in the Aegean Sea. 
+Folegandros' landscape is varied, and includes tall cliffs and a large cave. The "capital" of the island, Chora, is built on the edge of a 200-metre high cliff. 
+ Among the notable beaches on Folegandros is Katergo, accessible only by foot or by boat from Karavostasis. </p>
+</div>
+    })
+    (function Alonisos() {
+        <div id="pcontainer">
+  <p> Alonissos is an island in the Aegean Sea, located close to Skopelos, with lush green vegetation to the point that pine woods sometimes reach the numerous beaches on the island.
+ Alonissos island is part of the Sporades islands. Its surface is 66 square kilometres and has approximately 2700 local inhabitants. Alonissos in an isle of outstanding beauty with the most impressing abysses in the Aegean Sea. 
+ Alonissos is a peaceful island, quite lively on the Southern area, where there are a number of modest villages with narrow streets and picturesque buildings and houses. Patitiri, the capital of the island, located in this area, offers a variety of transportation services connecting the island with the other islands nearby and the mainland.</p>
+</div>
+    })
+    (function Syros() {
+        <div id="pcontainer">
+  <p>Syros (/ˈsaɪrɒs, -roʊs/; Greek: Σύρος), or Siros or Syra is a Greek island in the Cyclades, in the Aegean Sea. It is located 78 nautical miles (144 km) south-east of Athens. The area of the island is 83.6 km2 (32 sq mi) and it has 21,507 inhabitants (2011 census).[1]
+
+The largest towns are Ermoupoli, Ano Syros, and Vari (el). Ermoupoli is the capital of the island and of the Cyclades. It has always been a significant port town, and during the 19th century it was even more significant than Piraeus. Other villages are Galissas, Foinikas, Pagos, Manna, Kini and Poseidonia </p>
+</div>
+    })
+    (function Milos() {
+        <div id="pcontainer">
+  <p>he volcanic activity in ancient years has endowed Milos island with an exciting variety of gorgeous landscapes, consequently offering the visitor a wide range of activities. The funny shapes of the rocks and their wonderful colours at the beautiful white sandy beaches are one expression of the volcanic features of Mílos; the economic activity is another: minerals such as obsidian are excavated here. 
+      What is more, one of the most ancient mines in the Mediterranean is on this island.</p>
+</div>
+    })
 
     function clearValidationMessages() {
         FullName.setCustomValidity("");
